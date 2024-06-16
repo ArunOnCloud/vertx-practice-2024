0b187879d09a0509983570658b6560bb9bf21ccd
aram <aram@roku.com>
2024-06-16 22:34:18 +0530

    first working code

--- temperature-sensors/src/main/java/com/aram/practice/sensors/Main.java
+++ temperature-sensors/src/main/java/com/aram/practice/sensors/Main.java
@@ -2,16 +2,41 @@ package com.aram.practice.sensors;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//public class Main extends AbstractVerticle {
+public class Main  {
+
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.printf("Hello and welcome!");
 
-    for (int i = 1; i <= 5; i++) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      System.out.println("i = " + i);
-    }
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new SensorVerticle());
+    logger.info(" Main method verticle");
+
   }
+
+//  @Override
+//  public void start(Promise<Void> startPromise) throws Exception {
+//    vertx.createHttpServer().requestHandler(req -> {
+//      req.response()
+//              .putHeader("content-type", "text/plain")
+//              .end("Hello from Vert.x!");
+//    }).listen(8888, http -> {
+//      if (http.succeeded()) {
+//        startPromise.complete();
+//        System.out.println("HTTP server started on port 8888");
+//      } else {
+//        startPromise.fail(http.cause());
+//      }
+//    });
+//  }
 }
